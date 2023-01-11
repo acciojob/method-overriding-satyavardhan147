f53b308932aabf261074aade391939747aa05280
satyavardhan147 <59812028+satyavardhan147@users.noreply.github.com>
2023-01-11 18:29:22 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
+import java.io.*;
+import java.util.*;
 
+class A
+{
+   public String meth()
+   {
+     return "Invoking method from class A";
+   }
+}
+
+class B extends A
+{    
+    @Override
+    public String meth()
+    {
+       return "Method is overridden in Extendend class B";
+    }
+
+}
 public class Main {
-  
+  B bObj = new B();
+  bObj.meth();
+  bObj.meth();
 }
